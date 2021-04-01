7a0d001ef1749039956cb2381214e33fb7b6a7fc
mrkai1253 <asishshajithomas@gmail.com>
2021-04-01 16:05:06 +0530

    bug fixed: Deletion of shutdown

--- app.py
+++ app.py
@@ -3,35 +3,12 @@ from utils import helper
 import pandas as pd
 
 
-def createDFs():
-    try:
-        authDf = pd.read_csv("./data/auth.csv")
-        authDf = authDf.loc[:, ~authDf.columns.str.contains('^Unnamed')]
-
-        employeeDf = pd.read_csv("./data/employees.csv")
-        employeeDf = employeeDf.loc[:, ~
-                                    employeeDf.columns.str.contains('^Unnamed')]
-
-    except pd.errors.EmptyDataError:
-        authDf = pd.DataFrame(
-            columns=["Name", "Password", "Designation"])
-        authDf = authDf.loc[:, ~authDf.columns.str.contains('^Unnamed')]
-        authDf.to_csv("./data/auth.csv")
-
-        employeeDf = pd.DataFrame(
-            columns=["Name", "Post", "Salary", "Department"])
-        employeeDf = employeeDf.loc[:, ~
-                                    employeeDf.columns.str.contains('^Unnamed')]
-        employeeDf.to_csv("./data/employees.csv")
-    return authDf, employeeDf
-
-
 def main():
 
     authenticated = False
     loggedInUser = None
 
-    authDf, employeeDf = createDFs()
+    authDf, employeeDf = helper.createDFs()
 
     while True:
         print("---------------------------------")
